88b631ebb455e8146b36886b614e40746936d69a
dastanchick <dosyafree32@gmail.com>
2022-11-15 21:36:33 +0600

    I am

--- src/Food.java
+++ src/Food.java
@@ -1,18 +1,15 @@
 public class Food {
     String name;
-
     public Food(String name, String taste, String ingredient) {
         this.name = name;
         this.taste = taste;
         this.ingredient = ingredient;
     }
-
     String taste;
     String ingredient;
     public static String minus(String name){
         return name;
     }
-
     @Override
     public String toString() {
         return "Food{" +
@@ -21,7 +18,6 @@ public class Food {
                 ", ingredient='" + ingredient + '\'' +
                 '}';
     }
-
     public static String plus(String name){
         return name;
     }

755a226e22301e251fe66f84b8af570bc26322c0
dastanchick <dosyafree32@gmail.com>
2022-11-15 21:34:13 +0600

    Initial commit

---
+++ src/Food.java
@@ -0,0 +1,28 @@
+public class Food {
+    String name;
+
+    public Food(String name, String taste, String ingredient) {
+        this.name = name;
+        this.taste = taste;
+        this.ingredient = ingredient;
+    }
+
+    String taste;
+    String ingredient;
+    public static String minus(String name){
+        return name;
+    }
+
+    @Override
+    public String toString() {
+        return "Food{" +
+                "name='" + name + '\'' +
+                ", taste='" + taste + '\'' +
+                ", ingredient='" + ingredient + '\'' +
+                '}';
+    }
+
+    public static String plus(String name){
+        return name;
+    }
+}
---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+      Food food = new Food("хотдог","spicy","sausage");
+        System.out.println(food);
+        Food delicious = new Food("pilaf","oil","rice");
+        System.out.println(delicious);
+        Food food1 = new Food("Shawarma","appetizing","meat");
+        System.out.println(food1);
+        Food food2 = new Food("ice cream","cold","sugar");
+        System.out.println(food2);
+        Food food3 = new Food("samsa","spicy","dough");
+        System.out.println(food3);
+    }
+}
